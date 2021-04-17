25b4de7f70e902e080afec5a4a8d640a4df35214
Gur Telem <gur111telem@gmail.com>
2021-04-18 00:43:11 +0300

    organize main func

--- smash.cpp
+++ smash.cpp
@@ -1,25 +1,54 @@
-#include <iostream>
-#include <unistd.h>
-#include <sys/wait.h>
 #include <signal.h>
+#include <sys/wait.h>
+#include <unistd.h>
+
+#include <iostream>
+
 #include "Commands.h"
 #include "signals.h"
 
+using std::cin;
+using std::cout;
+using std::flush;
+using std::getline;
+using std::string;
+
 int main(int argc, char* argv[]) {
-    if(signal(SIGTSTP , ctrlZHandler)==SIG_ERR) {
-        perror("smash error: failed to set ctrl-Z handler");
+    if (signal(SIGTSTP, ctrlZHandler) == SIG_ERR) {
+        perror(ERR_PREFIX "failed to set ctrl-Z handler");
     }
-    if(signal(SIGINT , ctrlCHandler)==SIG_ERR) {
-        perror("smash error: failed to set ctrl-C handler");
+    if (signal(SIGINT, ctrlCHandler) == SIG_ERR) {
+        perror(ERR_PREFIX "failed to set ctrl-C handler");
     }
 
-    //TODO: setup sig alarm handler
+    // TODO: setup sig alarm handler
 
     SmallShell& smash = SmallShell::getInstance();
-    while(true) {
-        std::cout << "smash> ";
-        std::string cmd_line;
-        std::getline(std::cin, cmd_line);
+    string cmd_line = "";
+    int c;
+    while (true) {
+        cout << smash.getPrompt() << flush;
+        // switch ((c = getch())) {
+        //     case KEY_UP:
+        //         cout << endl << "Up" << endl;  // key up
+        //         break;
+        //     case KEY_DOWN:
+        //         cout << endl << "Down" << endl;  // key down
+        //         break;
+        //     case KEY_LEFT:
+        //         cout << endl << "Left" << endl;  // key left
+        //         break;
+        //     case KEY_RIGHT:
+        //         cout << endl << "Right" << endl;  // key right
+        //         break;
+        //     case '\n':
+        //         smash.executeCommand(cmd_line.c_str());
+        //         break;
+        //     default:
+        //         cmd_line += c;
+        //         break;
+        // }
+        getline(cin, cmd_line);
         smash.executeCommand(cmd_line.c_str());
     }
     return 0;

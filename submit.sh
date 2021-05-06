e74b4e002e8dae9d4387750a37a5e7f35cc0cc59
Gur Telem <gur111telem@gmail.com>
2021-05-06 17:03:39 +0300

    Modify submit script to include time in filename

--- submit.sh
+++ submit.sh
@@ -1,4 +1,9 @@
-zip submission.zip Commands.cpp Jobs.cpp SmallShell.cpp Utils.cpp main.cpp signals.cpp \
+var=$(date +"%FORMAT_STRING")
+now=$(date +"%Y%m%d_%H%M")
+printf "%s\n" $now
+
+
+zip submission_$now.zip Commands.cpp Jobs.cpp SmallShell.cpp Utils.cpp main.cpp signals.cpp \
     Commands.h Constants.h Exceptions.h Jobs.h SmallShell.h Utils.h signals.h \
     submitters.txt Makefile
 
